a5473ad9c990d279aa7a3959d103b805a9be5107
Churkin Andrey <andrey.churkin@devexpress.com>
2015-08-06 16:39:35 +0300

    Correct string-trim.js comment

--- src/utils/string-trim.js
+++ src/utils/string-trim.js
@@ -3,15 +3,9 @@
 // Do not use any browser or node-specific API!
 // -------------------------------------------------------------
 
-// NOTE: Some web-sites override String.prototype.trim method:
-//
-//      String.prototype.trim = function() {
-//          return this.replace(/^\s/, '');
-//      };
-//
-// In our scripts we are using 'trim' function and look forward to its default behavior.
-// In order to protect us from spoofing we must use the our own function implementation.
-
+// NOTE: Some websites override the String.prototype.trim method. When we use this function
+// in our scripts, we expect it to have the default behavior. Therefore, in order to protect
+// ourselves from spoofing, we must use our own implementation.
 
 export default function trim (str) {
     return str.replace(/^\s+|\s+$/g, '');
