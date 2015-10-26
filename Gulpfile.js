3ecd2fc16541a3b49a482f9194327e0b10f94d8f
Churkin Andrey <andrey.churkin@devexpress.com>
2015-10-26 14:42:12 +0300

    Web platform tests integration. Part 1 - Base.

--- Gulpfile.js
+++ Gulpfile.js
@@ -197,4 +197,10 @@ gulp.task('playground', ['build'], function () {
     return hang();
 });
 
+gulp.task('web-platform', ['build'], function () {
+    require('./test/web-platform/server').start();
+
+    return hang();
+});
+
 gulp.task('travis', [process.env.GULP_TASK || '']);
