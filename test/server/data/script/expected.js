3ebab57ed95da0071328cd02a1ebaf0759a423d3
churkin <alam17@mail.ru>
2016-04-26 17:38:26 +0300

    js error on the http://www.liveinternet.ru/ (close #558) (#559)

--- test/server/data/script/expected.js
+++ test/server/data/script/expected.js
@@ -1,2 +1,6 @@
 /*hammerhead|script-processing-header|start*/var __w$= typeof window!=="undefined"&&window;__w$ && __w$["hammerhead|process-dom-method"] && __w$["hammerhead|process-dom-method"]();var __get$Loc=__w$?__w$.__get$Loc:function(l){return l},__set$Loc=__w$?__w$.__set$Loc:function(l,v){return l = v},__set$=__w$?__w$.__set$:function(o,p,v){return o[p] = v},__get$=__w$?__w$.__get$:function(o,p){return o[p]},__call$=__w$?__w$.__call$:function(o,p,a){return o[p].apply(o,a)},__get$Eval=__w$?__w$.__get$Eval:function(e){return e},__proc$Script=__w$?__w$.__proc$Script:function(s){return s};/*hammerhead|script-processing-header|end*/
-__set$(window,"location",'test');(function(){return __set$Loc(location,'test')||(location='test');}.apply(this));__set$(document,"location",'test');__set$(document,"domain",'test');__set$(document,"cookie",'test');
+__set$(window, "location", 'test');
+(function () { return __set$Loc(location, 'test') || (location = 'test'); }.apply(this));
+__set$(document, "location", 'test');
+__set$(document, "domain", 'test');
+__set$(document, "cookie", 'test');
