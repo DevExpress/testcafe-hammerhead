DevExpress/testcafe-hammerhead

commit a3193139ad63e164ec9a38c3d091e9c624b11789

node.js v5.11 support (#557)

src/client/sandbox/code-instrumentation/properties/index.js
@@ -1,4 +1,5 @@
 import INTERNAL_PROPS from '../../../../processing/dom/internal-properties';
+import { SAME_ORIGIN_CHECK_FAILED_STATUS_CODE } from '../../../../request-pipeline/xhr/same-origin-policy';
 import SHADOW_UI_CLASSNAME from '../../../../shadow-ui/class-name';
 import LocationAccessorsInstrumentation from '../location';
 import LocationWrapper from '../location/wrapper';
@@ -184,8 +185,10 @@ export default class PropertyAccessorsInstrumentation extends SandboxBase {
                 get: el => {
                     if (domUtils.isScriptElement(el))
                         return getPendingElementContent(el) || removeProcessingHeader(el.innerHTML);
-                    else if (domUtils.isStyleElement(el))
-                        return getPendingElementContent(el) || styleProcessor.cleanUp(el.innerHTML, urlUtils.parseProxyUrl);
+                    else if (domUtils.isStyleElement(el)) {
+                        return getPendingElementContent(el) ||
+                               styleProcessor.cleanUp(el.innerHTML, urlUtils.parseProxyUrl);
+                    }
 
                     return cleanUpHtml(el.innerHTML, el.tagName);
                 },
@@ -243,8 +246,10 @@ export default class PropertyAccessorsInstrumentation extends SandboxBase {
                 get: el => {
                     if (domUtils.isScriptElement(el))
                         return getPendingElementContent(el) || removeProcessingHeader(el.innerText);
-                    else if (domUtils.isStyleElement(el))
-                        return getPendingElementContent(el) || styleProcessor.cleanUp(el.innerText, urlUtils.parseProxyUrl);
+                    else if (domUtils.isStyleElement(el)) {
+                        return getPendingElementContent(el) ||
+                               styleProcessor.cleanUp(el.innerText, urlUtils.parseProxyUrl);
+                    }
                 },
 
                 set: (el, text) => {
@@ -503,8 +508,10 @@ export default class PropertyAccessorsInstrumentation extends SandboxBase {
                 get: el => {
                     if (domUtils.isScriptElement(el))
                         return getPendingElementContent(el) || removeProcessingHeader(el.textContent);
-                    else if (domUtils.isStyleElement(el))
-                        return getPendingElementContent(el) || styleProcessor.cleanUp(el.textContent, urlUtils.parseProxyUrl);
+                    else if (domUtils.isStyleElement(el)) {
+                        return getPendingElementContent(el) ||
+                               styleProcessor.cleanUp(el.textContent, urlUtils.parseProxyUrl);
+                    }
                 },
 
                 set: (el, text) => {
@@ -555,6 +562,16 @@ export default class PropertyAccessorsInstrumentation extends SandboxBase {
                 condition: domUtils.isDocument,
                 get:       doc => ShadowUI._filterNodeList(doc.scripts),
                 set:       (doc, value) => doc.scripts = value
+            },
+
+            status: {
+                condition: domUtils.isXhr,
+                // NOTE: The browser returns a 0 status code if the same-origin policy check is failed. Node.js v5.11 or higher
+                // (https://github.com/nodejs/node/blob/v5.11.0/CHANGELOG.md, https://github.com/nodejs/node/pull/6291/files)
+                // does not allow returning a response with this code. So, we use a valid unused 222 status code and change
+                // it to 0 on the client side.
+                get:       xhr => xhr.status === SAME_ORIGIN_CHECK_FAILED_STATUS_CODE ? 0 : xhr.status,
+                set:       (xhr, value) => xhr.status = value
             },
 
             // Event

src/client/utils/dom.js
@@ -482,6 +482,10 @@ export function isShadowUIElement (element) {
     return false;
 }
 
+export function isXhr (instance) {
+    return instance instanceof window.XMLHttpRequest || instance.toString() === '[object XMLHttpRequest]';
+}
+
 export function isWindow (instance) {
     if (instance instanceof nativeMethods.windowClass)
         return true;

test/client/fixtures/sandbox/xhr-test.js
@@ -93,6 +93,16 @@ test('createNativeXHR returns an xhr with the native "open" method (GH-492)', fu
     XhrSandbox.createNativeXHR().open();
 });
 
+test('the internal 222 status code should be replaced to 0 on the client side', function () {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', '/xhr-222/', false);
+    xhr.send();
+
+    strictEqual(xhr.response || xhr.responseText, 'true');
+    strictEqual(getProperty(xhr, 'status'), 0);
+});
+
 if (!browserUtils.isIE9) {
     asyncTest('send the origin header correctly (GH-284)', function () {
         var xhrTestFunc = function () {

test/client/fixtures/utils/dom-test.js
@@ -65,6 +65,21 @@ test('isDomElement', function () {
     /* eslint-enable no-extra-parens */
 });
 
+asyncTest('isXhr', function () {
+    var iframe = document.createElement('iframe');
+
+    iframe.id  = 'test5';
+    iframe.src = '';
+    ok(domUtils.isXhr(new window.XMLHttpRequest()));
+    window.QUnitGlobals.waitForIframe(iframe)
+        .then(function () {
+            ok(domUtils.isXhr(new iframe.contentWindow.XMLHttpRequest()));
+            iframe.parentNode.removeChild(iframe);
+            start();
+        });
+    document.body.appendChild(iframe);
+});
+
 asyncTest('isDomElement for iframe elements', function () {
     var iframe = document.createElement('iframe');
 
@@ -182,7 +197,7 @@ test('isSVGElement', function () {
 asyncTest('isSVGElement for iframe elements', function () {
     var iframe = document.createElement('iframe');
 
-    iframe.id  = 'test_unique_id_ydnfgf544332';
+    iframe.id = 'test_unique_id_ydnfgf544332';
     window.QUnitGlobals.waitForIframe(iframe)
         .then(function () {
             var iframeDocument = iframe.contentDocument;

src/request-pipeline/index.js
@@ -4,7 +4,7 @@ import * as headerTransforms from './header-transforms';
 import { process as processResource } from '../processing/resources';
 import { MESSAGE, getText } from '../messages';
 import connectionResetGuard from './connection-reset-guard';
-import { check as checkSameOriginPolicy } from './xhr/same-origin-policy';
+import { check as checkSameOriginPolicy, SAME_ORIGIN_CHECK_FAILED_STATUS_CODE } from './xhr/same-origin-policy';
 import { fetchBody, respond404 } from '../utils/http';
 import { inject as injectUpload } from '../upload';
 import { isSpecialPage } from '../utils/url';
@@ -38,7 +38,7 @@ var stages = {
 
     2: function checkSameOriginPolicyCompliance (ctx, next) {
         if (ctx.isXhr && !checkSameOriginPolicy(ctx)) {
-            ctx.closeWithError(0);
+            ctx.closeWithError(SAME_ORIGIN_CHECK_FAILED_STATUS_CODE);
             return;
         }
 

src/processing/script/instrumented.js
@@ -55,6 +55,7 @@ export const PROPERTIES = [
     'search',
     'sessionStorage',
     'src',
+    'status',
     'target',
     'text',
     'textContent',

src/request-pipeline/xhr/same-origin-policy.js
@@ -1,5 +1,7 @@
 import XHR_HEADERS from './headers';
 
+export const SAME_ORIGIN_CHECK_FAILED_STATUS_CODE = 222;
+
 // NOTE: https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS
 export function check (ctx) {
     var reqOrigin = ctx.dest.reqOrigin;

test/client/config-qunit-server-app.js
@@ -59,6 +59,11 @@ module.exports = function (app) {
         res.send(req.headers['x-hammerhead|xhr|origin']);
     });
 
+    app.get('/xhr-222/', function (req, res) {
+        res.statusCode = 222;
+        res.send('true');
+    });
+
     app.post('/form-data', function (req, res) {
         var chunks = [];
 

commit 23a9450fc5f5529385e75548b9573c11a4d49cc9

fix health monitor regression after #557

src/client/sandbox/code-instrumentation/properties/index.js
@@ -6,6 +6,7 @@ import LocationWrapper from '../location/wrapper';
 import SandboxBase from '../../base';
 import UploadSandbox from '../../upload';
 import ShadowUI from '../../shadow-ui';
+import XhrSandbox from '../../xhr';
 import * as destLocation from '../../../utils/destination-location';
 import * as domUtils from '../../../utils/dom';
 import * as typeUtils from '../../../utils/types';
@@ -565,7 +566,7 @@ export default class PropertyAccessorsInstrumentation extends SandboxBase {
             },
 
             status: {
-                condition: domUtils.isXhr,
+                condition: XhrSandbox.isOpenedXhr,
                 // NOTE: The browser returns a 0 status code if the same-origin policy check is failed. Node.js v5.11 or higher
                 // (https://github.com/nodejs/node/blob/v5.11.0/CHANGELOG.md, https://github.com/nodejs/node/pull/6291/files)
                 // does not allow returning a response with this code. So, we use a valid unused 222 status code and change

src/client/sandbox/xhr.js
@@ -4,6 +4,8 @@ import { getProxyUrl } from '../utils/url';
 import XHR_HEADERS from '../../request-pipeline/xhr/headers';
 import { getOrigin } from '../utils/destination-location';
 
+const IS_OPENED_XHR = 'hammerhead|xhr|is-opened-xhr';
+
 // NOTE: We should avoid using native object prototype methods,
 // since they can be overriden by the client code. (GH-245)
 var arraySlice = Array.prototype.slice;
@@ -19,6 +21,10 @@ export default class XhrSandbox extends SandboxBase {
         var xhr = new nativeMethods.XMLHttpRequest();
 
         this.corsSupported = typeof xhr.withCredentials !== 'undefined';
+    }
+
+    static isOpenedXhr (obj) {
+        return obj[IS_OPENED_XHR];
     }
 
     static createNativeXHR () {
@@ -50,6 +56,8 @@ export default class XhrSandbox extends SandboxBase {
         // NOTE: Redirect all requests to the Hammerhead proxy and ensure that requests don't
         // violate Same Origin Policy.
         xmlHttpRequestProto.open = function () {
+            this[IS_OPENED_XHR] = true;
+
             // NOTE: Emulate CORS, so that 3rd party libs (e.g. jQuery) allow requests with the proxy host as well as
             // the destination page host.
             if (!xhrSandbox.corsSupported)

src/client/utils/dom.js
@@ -482,10 +482,6 @@ export function isShadowUIElement (element) {
     return false;
 }
 
-export function isXhr (instance) {
-    return instance instanceof window.XMLHttpRequest || instance.toString() === '[object XMLHttpRequest]';
-}
-
 export function isWindow (instance) {
     if (instance instanceof nativeMethods.windowClass)
         return true;

test/client/fixtures/sandbox/xhr-test.js
@@ -97,6 +97,7 @@ test('the internal 222 status code should be replaced to 0 on the client side', 
     var xhr = new XMLHttpRequest();
 
     xhr.open('GET', '/xhr-222/', false);
+    ok(XhrSandbox.isOpenedXhr(xhr));
     xhr.send();
 
     strictEqual(xhr.response || xhr.responseText, 'true');

test/client/fixtures/utils/dom-test.js
@@ -65,21 +65,6 @@ test('isDomElement', function () {
     /* eslint-enable no-extra-parens */
 });
 
-asyncTest('isXhr', function () {
-    var iframe = document.createElement('iframe');
-
-    iframe.id  = 'test5';
-    iframe.src = '';
-    ok(domUtils.isXhr(new window.XMLHttpRequest()));
-    window.QUnitGlobals.waitForIframe(iframe)
-        .then(function () {
-            ok(domUtils.isXhr(new iframe.contentWindow.XMLHttpRequest()));
-            iframe.parentNode.removeChild(iframe);
-            start();
-        });
-    document.body.appendChild(iframe);
-});
-
 asyncTest('isDomElement for iframe elements', function () {
     var iframe = document.createElement('iframe');
 