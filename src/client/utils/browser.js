e14757226e5fbd21189c659f99e38e0bdeeeaf31
Mikhail Losev <miherlosev@mail.ru>
2016-10-27 16:28:33 +0300

    fix 'Wrong version number for Chrome' (close #777). (#781)
    * fix 'Wrong version number for Chrome' (close #777)
    
    * add 'p' letter to the regexp variable name

--- src/client/utils/browser.js
+++ src/client/utils/browser.js
@@ -1,48 +1,50 @@
-function getMSEdgeVersion (userAgent) {
-    var edgeStrIndex = userAgent.indexOf('edge/');
-
-    return parseInt(userAgent.substring(edgeStrIndex + 5, userAgent.indexOf('.', edgeStrIndex)), 10);
-}
-
 function calculateBrowserAndVersion (userAgent) {
-    var webkitRegEx  = /(webkit)[ \/]([\w.]+)/;
-    var msieRegEx    = /(msie) ([\w.]+)/;
-    var firefoxRegEx = /(firefox)/;
-
-    var match = webkitRegEx.exec(userAgent) ||
-                msieRegEx.exec(userAgent) ||
-                userAgent.indexOf('compatible') < 0 && firefoxRegEx.exec(userAgent) ||
-                [];
+    var msEdgeRegExp   = /(edge)\/([\w.]+)/;
+    var chromeRegExp   = /(chrome)[\/]([\w.]+)/;
+    var safariRegExp   = /(webkit)[ \/]([\w.]+).*(version)[ \/]([\w.]+).*(safari)[ \/]([\w.]+)/;
+    var webkitRegExp   = /(webkit)[\/]([\w.]+)/;
+    var ie9And10RegExp = /(msie) ([\w.]+)/;
+    var ie11RegExp     = /(rv)(?::| )([\w.]+)/;
+    var firefoxRegExp  = /(mozilla)(?:.*? rv:([\w.]+)|)/;
+    var match          = msEdgeRegExp.exec(userAgent) ||
+                         chromeRegExp.exec(userAgent) ||
+                         safariRegExp.exec(userAgent) ||
+                         webkitRegExp.exec(userAgent) ||
+                         ie9And10RegExp.exec(userAgent) ||
+                         userAgent.indexOf('trident') >= 0 && ie11RegExp.exec(userAgent) ||
+                         userAgent.indexOf('compatible') < 0 && firefoxRegExp.exec(userAgent) ||
+                         [];
 
     return {
-        name:    match[1] || '',
-        version: match[2] || '0'
+        name:    match[5] || match[3] || match[1] || '',
+        version: parseInt(match[4] || match[2] || '0', 10)
     };
 }
 
-var userAgent           = navigator.userAgent.toLowerCase();
-var browser             = calculateBrowserAndVersion(userAgent);
-var majorBrowserVersion = parseInt(browser.version, 10);
+var userAgent = navigator.userAgent.toLowerCase();
+var browser   = calculateBrowserAndVersion(userAgent);
 
-export var isIE11 = /trident\/7.0/.test(userAgent) && !(browser.name === 'msie' &&
-                    (majorBrowserVersion === 9 || majorBrowserVersion === 10));
+//Platforms
+export var isMacPlatform = /^Mac/.test(navigator.platform);
+export var isAndroid     = /(android)/.test(userAgent);
+export var isIOS         = /(iphone|ipod|ipad)/.test(userAgent);
 
-if (isIE11)
-    majorBrowserVersion = 11;
-
-export var isAndroid         = /android/.test(userAgent);
-export var isMSEdge          = !!/edge\//.test(userAgent);
-export var version           = isMSEdge ? getMSEdgeVersion(userAgent) : majorBrowserVersion;
-export var isIOS             = /(iphone|ipod|ipad)/.test(userAgent);
-export var isIE              = browser.name === 'msie' || isIE11 || isMSEdge;
+//Browsers
+export var version           = browser.version;
+export var isIE11            = browser.name === 'rv';
+export var isIE              = browser.name === 'msie' || browser.name === 'edge' || isIE11;
 export var isIE10            = isIE && version === 10;
 export var isIE9             = isIE && version === 9;
-export var isFirefox         = browser.name === 'firefox' && !isIE11;
-export var isSafari          = isIOS || /safari/.test(userAgent) && !/chrome/.test(userAgent);
-export var isWebKit          = browser.name === 'webkit' && !isMSEdge;
-export var hasTouchEvents    = !!('ontouchstart' in window);
-export var isMacPlatform     = /^Mac/.test(navigator.platform);
+export var isFirefox         = browser.name === 'mozilla';
+export var isMSEdge          = browser.name === 'edge';
+export var isChrome          = browser.name === 'chrome';
+export var isSafari          = isIOS || browser.name === 'safari';
+export var isWebKit          = isChrome || isSafari || browser.name === 'webkit';
 
+//Feature detection
+export var hasTouchEvents = !!('ontouchstart' in window);
 // NOTE: We need to check touch points only for IE, because it has PointerEvent and MSPointerEvent (IE10, IE11)
 // instead of TouchEvent (T109295).
 export var isTouchDevice = hasTouchEvents || isIE && (navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
+
+
