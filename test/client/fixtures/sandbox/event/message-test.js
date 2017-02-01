0a803009b4f12efa8e16b1f9c9a7436caa79b6c6
Andrey Churkin <alam17@mail.ru>
2017-02-01 13:46:46 +0300

    Iframe with 'about:blank' src can not send 'postMessage'(close #1026) (#1028)
    * Iframe with 'about:blank' src can not send 'postMessage'(close #1026)
    
    * fix miherlosev's remarks

--- test/client/fixtures/sandbox/event/message-test.js
+++ test/client/fixtures/sandbox/event/message-test.js
@@ -343,6 +343,25 @@ asyncTest('timeout (added to DOM iframe)', function () {
 
 module('regression');
 
+asyncTest('send message from iframe with "about:blank" src (GH-1026)', function () {
+    var iframe = document.createElement('iframe');
+    var src    = 'javascript:\'<html><body><script>window.parent.postMessage("gh1026", "*")</sc' +
+                 'ript></body></html>\'';
+
+    iframe.id = 'test-' + Date.now;
+    setProperty(iframe, 'src', src);
+    setProperty(window, 'onmessage', function (e) {
+        if (e.data === 'gh1026') {
+            iframe.parentNode.removeChild(iframe);
+            window.onmessage = null;
+            ok(true);
+            start();
+        }
+    });
+
+    document.body.appendChild(iframe);
+});
+
 asyncTest('service messages from embedded iframe (GH-803)', function () {
     var iframe = document.createElement('iframe');
 
