c13baefbb02ffd15eba0876428cbc84350c7742b
Andrey Belym <belym.a.2105@gmail.com>
2017-02-13 11:49:11 +0300

    Fix require() on Electron clients (closes #1038) (#1039)
    * Fix require() on Electron clients (closes #1038)
    
    * Rename the function

--- src/client/sandbox/electron.js
+++ src/client/sandbox/electron.js
@@ -1,5 +1,6 @@
 import SandboxBase from './base';
 import { processScript } from '../../processing/script';
+import * as destinationLocation from '../utils/destination-location';
 
 export default class ElectronSandbox extends SandboxBase {
     static _createFnWrapper (vm, nativeFn) {
@@ -9,6 +10,28 @@ export default class ElectronSandbox extends SandboxBase {
 
             return nativeFn.apply(vm, args);
         };
+    }
+
+    static _overrideElectronModulePaths (window) {
+        // NOTE: Need this to avoid Webmake require
+        /* eslint-disable no-eval */
+        var electronRequire = eval('require');
+        /* eslint-enable no-eval */
+
+        var path        = electronRequire('path');
+        var destination = destinationLocation.getParsed();
+
+        if (destination.protocol !== 'file:')
+            return;
+
+        var pathname = window.process.platform === 'win32' && destination.pathname[0] === '/' ? destination.pathname.substr(1) : destination.pathname;
+
+        window.__filename = path.normalize(decodeURIComponent(pathname));
+        window.__dirname = path.dirname(window.__filename);
+
+        window.module.filename = window.__filename;
+
+        window.module.paths = window.module.paths.concat(electronRequire('module')._nodeModulePaths(window.__dirname));
     }
 
     attach (window) {
@@ -30,6 +53,8 @@ export default class ElectronSandbox extends SandboxBase {
             vm.runInContext      = ElectronSandbox._createFnWrapper(vm, nativeMethods.runInContext);
             vm.runInNewContext   = ElectronSandbox._createFnWrapper(vm, nativeMethods.runInNewContext);
             vm.runInThisContext  = ElectronSandbox._createFnWrapper(vm, nativeMethods.runInThisContext);
+
+            ElectronSandbox._overrideElectronModulePaths(window);
         }
     }
 }
