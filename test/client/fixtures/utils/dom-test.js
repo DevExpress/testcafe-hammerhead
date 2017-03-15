0c6f462f684a2e656e2254030f42ef88c9f5db7b
Andrey Belym <belym.a.2105@gmail.com>
2017-03-15 17:06:10 +0300

    Get activeElement from Shadow DOM

--- test/client/fixtures/utils/dom-test.js
+++ test/client/fixtures/utils/dom-test.js
@@ -813,3 +813,21 @@ test('inspect html elements', function () {
     ok(domUtils.isTableElement(nativeMethods.createElement.call(document, 'table')), 'table');
     ok(domUtils.isTableDataCellElement(nativeMethods.createElement.call(document, 'td')), 'td');
 });
+
+if (browserUtils.isChrome) {
+    test('should return active element inside shadow DOM', function () {
+        var host  = document.createElement('div');
+        var root  = host.createShadowRoot();
+        var input = document.createElement('input');
+
+        document.body.appendChild(host);
+        root.appendChild(input);
+
+        nativeMethods.focus.call(input);
+
+        strictEqual(domUtils.getActiveElement(), input);
+
+        document.body.removeChild(host);
+    });
+}
+
