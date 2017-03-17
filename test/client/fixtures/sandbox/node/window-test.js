f21e23142a3ef091db43a9036a6e61c2a44bfab0
Andrey Churkin <andrey.churkin@devexpress.com>
2017-03-17 11:26:10 +0300

    Override  function (close #1035)

--- test/client/fixtures/sandbox/node/window-test.js
+++ test/client/fixtures/sandbox/node/window-test.js
@@ -112,6 +112,21 @@ if (isWorkerFromBlobSupported) {
     });
 }
 
+asyncTest('Navigator.sendBeacon must be overriden (GH-1035)', function () {
+    var originUrl    = 'http://example.com/index.html';
+    var originData   = 'some data';
+    var nativeMethod = nativeMethods.sendBeacon;
+
+    nativeMethods.sendBeacon = function (url, data) {
+        strictEqual(url, urlUtils.getProxyUrl(originUrl));
+        strictEqual(data, originData);
+        nativeMethods.sendBeacon = nativeMethod;
+        start();
+    };
+
+    window.navigator.sendBeacon(originUrl, originData);
+});
+
 test('window.onerror must be overriden (B238830)', function () {
     var error     = false;
     var windowObj = window.Window;
