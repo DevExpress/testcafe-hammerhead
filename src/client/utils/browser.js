9835c50b4b61cde70f4e3f0d46089b3c47896b3b
Andrey Belym <belym.a.2105@gmail.com>
2017-05-22 12:30:38 +0300

    Export WebKit version in browser utils; Bump version (#1138)
    * Export WebKit version in browser utils
    
    * Fix flag

--- src/client/utils/browser.js
+++ src/client/utils/browser.js
@@ -1,7 +1,8 @@
 import bowser from 'bowser';
 
-var userAgent = navigator.userAgent.toLowerCase();
-var info      = bowser._detect(userAgent);
+var userAgent          = navigator.userAgent.toLowerCase();
+var info               = bowser._detect(userAgent);
+var webkitVersionMatch = userAgent.match(/applewebkit\/(\d+(:?\.\d+)*)/);
 
 //Helper
 export var compareVersions = bowser.compareVersions;
@@ -12,18 +13,19 @@ export var isAndroid     = !!info.android;
 export var isIOS         = !!info.ios;
 
 //Browsers
-export var version     = parseInt(info.version, 10);
-export var fullVersion = info.version;
-export var isIE        = !!(info.msie || info.msedge);
-export var isIE11      = isIE && version === 11;
-export var isIE10      = isIE && version === 10;
-export var isIE9       = isIE && version === 9;
-export var isFirefox   = !!info.firefox;
-export var isMSEdge    = !!info.msedge;
-export var isChrome    = !!info.chrome;
-export var isSafari    = !!info.safari;
-export var isWebKit    = !!(info.webkit || info.blink);
-export var isElectron  = /electron/gi.test(userAgent);
+export var version       = parseInt(info.version, 10);
+export var fullVersion   = info.version;
+export var webkitVersion = webkitVersionMatch && webkitVersionMatch[1] || '';
+export var isIE          = !!(info.msie || info.msedge);
+export var isIE11        = isIE && version === 11;
+export var isIE10        = isIE && version === 10;
+export var isIE9         = isIE && version === 9;
+export var isFirefox     = !!info.firefox;
+export var isMSEdge      = !!info.msedge;
+export var isChrome      = !!info.chrome;
+export var isSafari      = !!info.safari;
+export var isWebKit      = !!(info.webkit || info.blink);
+export var isElectron    = /electron/g.test(userAgent);
 
 //Feature detection
 export var hasTouchEvents = !!('ontouchstart' in window);
