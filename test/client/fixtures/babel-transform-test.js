5a1f1bff77891fa9c07074b2f3a648e373a8c39c
LavrovArtem <artjomlav@gmail.com>
2017-08-03 15:33:30 +0300

    /fixtures/babel-transform-test.js

--- test/client/fixtures/babel-transform-test.js
+++ test/client/fixtures/babel-transform-test.js
@@ -1,13 +1,13 @@
 module('regression');
 
-asyncTest('should not throw an error after for...of loop transformation (GH-1231)', function () {
+test('should not throw an error after for...of loop transformation (GH-1231)', function () {
     var iframe = document.createElement('iframe');
     var div    = document.createElement('div');
 
     iframe.id  = 'test' + Date.now();
     iframe.src = window.QUnitGlobals.getResourceUrl('../data/iframe/simple-iframe.html');
 
-    window.QUnitGlobals.waitForIframe(iframe)
+    var promise = window.QUnitGlobals.waitForIframe(iframe)
         .then(function () {
             iframe.contentDocument.body.appendChild(div);
             document.body.removeChild(iframe);
@@ -18,8 +18,9 @@ asyncTest('should not throw an error after for...of loop transformation (GH-1231
         })
         .then(function (err) {
             ok(!err, err);
-            start();
         });
 
     document.body.appendChild(iframe);
+
+    return promise;
 });
