3d2492c53be04fcc90ecb6716531f65c4fb68fcc
helen-dikareva <yelena.dikareva@yandex.ru>
2017-09-12 12:41:23 +0300

    qunit test added

--- test/client/fixtures/sandbox/selection-test.js
+++ test/client/fixtures/sandbox/selection-test.js
@@ -6,20 +6,20 @@ var isIE            = browserUtils.isIE;
 var isMobileBrowser = browserUtils.isIOS || browserUtils.isAndroid;
 var browserVersion  = browserUtils.version;
 
-var createTestInput = function () {
+var createTestInput = function (type, value) {
     var input = document.createElement('input');
 
-    input.setAttribute('type', 'email');
+    input.setAttribute('type', type);
 
     document.body.appendChild(input);
 
-    input.value = inputTestValue;
+    input.value = value;
 
     return input;
 };
 
 test('Set and get selection on input with "email" type', function () {
-    var input = createTestInput();
+    var input = createTestInput('email', inputTestValue);
 
     var testSelection = {
         start:     2,
@@ -41,16 +41,41 @@ test('Set and get selection on input with "email" type', function () {
 });
 
 test('Get selection on input with "email" type', function () {
-    var input     = createTestInput();
+    var input     = createTestInput('email', inputTestValue);
     var selection = selectionSandbox.getSelection(input);
 
     strictEqual(selection.start, browserUtils.isFirefox && browserVersion < 51 || isIE ? 0 : inputTestValue.length);
     strictEqual(selection.end, browserUtils.isFirefox && browserVersion < 51 || isIE ? 0 : inputTestValue.length);
 
-    if (isMobileBrowser || browserUtils.isSafari || browserUtils.isChrome && (browserUtils.isMacPlatform || browserVersion < 54))
+    if (isMobileBrowser || browserUtils.isSafari ||
+        browserUtils.isChrome && (browserUtils.isMacPlatform || browserVersion < 54))
         strictEqual(selection.direction, 'none');
     else if (!isIE)
         strictEqual(selection.direction, 'forward');
 
+    document.body.removeChild(input);
+});
+
+test('Focus should stay on input with "number" type after setting selection', function () {
+    var input = createTestInput('number', 123456789);
+
+    var testSelection = {
+        start:     2,
+        end:       5,
+        direction: 'backward'
+    };
+
+    input.focus();
+    input.setSelectionRange(testSelection.start, testSelection.end, testSelection.direction);
+
+    var selection = selectionSandbox.getSelection(input);
+
+    strictEqual(selection.start, testSelection.start);
+    strictEqual(selection.end, testSelection.end);
+
+    if (!isIE)
+        strictEqual(selection.direction, testSelection.direction);
+
+    strictEqual(document.activeElement, input);
     document.body.removeChild(input);
 });
