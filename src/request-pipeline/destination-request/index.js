641ed9c636e62cc46f023ae1112f703b337350cf
LavrovArtem <artjomlav@gmail.com>
2017-11-30 16:04:13 +0300

    fix `Hammerhead emit an error "Failed to find a DNS-record for the resource"` (#1384 part 2) (#1394)
    * fix `Hammerhead emit an error "Failed to find a DNS-record for the resource"` (#1384 part 2)
    
    * fix test for node 4

--- src/request-pipeline/destination-request/index.js
+++ src/request-pipeline/destination-request/index.js
@@ -121,7 +121,11 @@ export default class DestinationRequest extends EventEmitter {
     }
 
     _isSocketHangUpErr (err) {
-        return err.message && SOCKET_HANG_UP_ERR_RE.test(err.message);
+        return err.message && SOCKET_HANG_UP_ERR_RE.test(err.message) &&
+               // NOTE: Only for nodejs 4 error with a same message will be generated for different cases.
+               // This is why, we filter a 'SocketHangUpErr' by stack.
+               // Remove filtering by stack content after ending support of nodejs 4.
+               err.stack && err.stack.indexOf('createHangUpError') !== -1;
     }
 
     _onTimeout () {
@@ -135,7 +139,7 @@ export default class DestinationRequest extends EventEmitter {
         if (this._isSocketHangUpErr(err))
             this.emit('socketHangUp');
 
-        if (requestAgent.shouldRegressHttps(err, this.opts)) {
+        else if (requestAgent.shouldRegressHttps(err, this.opts)) {
             requestAgent.regressHttps(this.opts);
             this._send();
         }
