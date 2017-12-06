4cfdcb30935d00cce0ff513d6b9d1cb0d637dc03
miherlosev@mail.ru <Ein7Onai>
2017-12-06 12:04:41 +0300

    try to fix tests

--- test/client/fixtures/sandbox/event/unload-test.js
+++ test/client/fixtures/sandbox/event/unload-test.js
@@ -22,7 +22,8 @@ asyncTest('BEFORE_UNLOAD_EVENT must be called last (GH-400)', function () {
                 uploadEventCounter++;
             });
 
-            iframeWindow[INSTRUCTION.setProperty](iframeWindow, 'on' + unloadSandbox.beforeUnloadEventName, function () {
+            iframeWindow[INSTRUCTION.setProperty](iframeWindow, 'on' +
+                                                                unloadSandbox.beforeUnloadEventName, function () {
                 uploadEventCounter++;
             });
 
@@ -31,16 +32,21 @@ asyncTest('BEFORE_UNLOAD_EVENT must be called last (GH-400)', function () {
 });
 
 test('UNLOAD_EVENT should be called', function () {
-    var unloadSandbox        = hammerhead.sandbox.event.unload;
-    var unloadEventWasCalled = false;
-    var handler = function () {
+    var unloadSandbox          = hammerhead.sandbox.event.unload;
+    var unloadEventWasCalled   = false;
+    var handler                = function () {
         unloadEventWasCalled = true;
         hammerhead.off(hammerhead.EVENTS.unload, handler);
+    };
+    var storedSandboxDispose   = hammerhead.sandbox.dispose;
+    hammerhead.sandbox.dispose = function () {
     };
 
     hammerhead.on(hammerhead.EVENTS.unload, handler);
     unloadSandbox.emit(unloadSandbox.UNLOAD_EVENT);
     ok(unloadEventWasCalled);
+
+    hammerhead.sandbox.dispose = storedSandboxDispose;
 });
 
 if (browserUtils.isSafari && !browserUtils.isIOS) {
