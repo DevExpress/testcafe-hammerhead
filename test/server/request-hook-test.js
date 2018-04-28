596fbf03032cfa34eb0d064504f2f72ff7cd3c7e
Mikhail Losev <miherlosev@mail.ru>
2018-04-28 23:03:00 +0300

    Request filter rule custom function (#1594)
    * fix wrong result for RequestFilterRule for custom function
    
    * remove 'only' hooks
    
    * simplify tests

--- test/server/request-hook-test.js
+++ test/server/request-hook-test.js
@@ -184,8 +184,10 @@ describe('RequestFilterRule', () => {
             method: 'Post',
             isAjax: 'test'
         }).match(requestInfo)).to.be.false;
-        expect(new RequestFilterRule(function (request) {
-            return request.url === 'http://example.com' &&
+        expect(new RequestFilterRule(() => {}).match(requestInfo)).to.be.false;
+        expect(new RequestFilterRule(request => request.url === 'wrong_url').match(requestInfo)).to.be.false;
+        expect(new RequestFilterRule(request => {
+            return request.url === 'http://example.com/' &&
                    request.method === 'post' &&
                    request.isAjax &&
                    request.body === '{ test: true }' &&
