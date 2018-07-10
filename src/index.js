9ffc6e7dc3d14a3c6d3b619b5021ff2f98f5b564
miherlosev@mail.ru <Ein7Onai>
2018-07-10 18:02:30 +0300

    tmp

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ResponseMock from './request-pipeline/request-hooks/response-mock';
 import RequestFilterRule from './request-pipeline/request-hooks/request-filter-rule';
 import { processScript, isScriptProcessed } from './processing/script';
 import ConfigureResponseEventOptions from './session/events/configure-response-event-options';
+import SAME_ORIGIN_CHECK_FAILED_STATUS_CODE from './request-pipeline/xhr/same-origin-check-failed-status-code';
 
 export default {
     Proxy,
@@ -12,5 +13,6 @@ export default {
     isScriptProcessed,
     ResponseMock,
     RequestFilterRule,
-    ConfigureResponseEventOptions
+    ConfigureResponseEventOptions,
+    SAME_ORIGIN_CHECK_FAILED_STATUS_CODE
 };
