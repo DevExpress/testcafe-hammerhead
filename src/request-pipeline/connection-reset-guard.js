93337d318aa802468c6cf7009e5c1c5f21b635b3
Artem Lavrov <artjomlav@gmail.com>
2018-08-17 15:27:30 +0300

    fix `Tests sometimes (frequently) lose connection` (close #1647, related to DevExpress/testcafe#2711) (#1729)
    * fix `Tests sometimes (frequently) lose connection` (close #1647, related to DevExpress/testcafe#2711)
    
    * add comment

--- src/request-pipeline/connection-reset-guard.js
+++ src/request-pipeline/connection-reset-guard.js
@@ -1,12 +1,16 @@
 import domain from 'domain';
+import os from 'os';
 
 const connectionResetDomain = domain.create();
 
 connectionResetDomain.on('error', err => {
-    if (err.code !== 'ECONNRESET') {
-        connectionResetDomain.removeAllListeners('error');
-        throw new Error(err);
-    }
+    // NOTE: The nodejs throw the EPIPE error instead of the ECONNRESET error
+    // when the connection is broken in some cases on MacOS
+    if (err.code === 'ECONNRESET' || os.type() === 'Darwin' && err.code === 'EPIPE')
+        return;
+
+    connectionResetDomain.removeAllListeners('error');
+    throw err;
 });
 
 export default function (fn) {
