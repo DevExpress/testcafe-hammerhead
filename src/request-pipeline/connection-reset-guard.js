35208b29727ab2766e01cb0d84c7168860f0c4d9
Artem Lavrov <artjomlav@gmail.com>
2018-09-27 17:56:28 +0300

    fix `Tests sometimes (frequently) lose connection` (for Linux) (close  #1647) (#1768)
    * fix `Tests sometimes (frequently) lose connection` (for Linux) (close #1647)
    
    * fix remark
    
    * fix remark

--- src/request-pipeline/connection-reset-guard.js
+++ src/request-pipeline/connection-reset-guard.js
@@ -1,12 +1,12 @@
 import domain from 'domain';
-import os from 'os';
+import os from 'os-family';
 
 const connectionResetDomain = domain.create();
 
 connectionResetDomain.on('error', err => {
     // NOTE: The nodejs throw the EPIPE error instead of the ECONNRESET error
-    // when the connection is broken in some cases on MacOS
-    if (err.code === 'ECONNRESET' || os.type() === 'Darwin' && err.code === 'EPIPE')
+    // when the connection is broken in some cases on MacOS and Linux
+    if (err.code === 'ECONNRESET' || !os.win && err.code === 'EPIPE')
         return;
 
     connectionResetDomain.removeAllListeners('error');
