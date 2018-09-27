86f329b15b71b5349ebc23deede30eea2931a582
LavrovArtem <artjomlav@gmail.com>
2018-09-27 16:30:16 +0300

    fix remark

--- src/request-pipeline/connection-reset-guard.js
+++ src/request-pipeline/connection-reset-guard.js
@@ -1,12 +1,12 @@
 import domain from 'domain';
-import os from 'os';
+import osFamily from 'os-family';
 
 const connectionResetDomain = domain.create();
 
 connectionResetDomain.on('error', err => {
     // NOTE: The nodejs throw the EPIPE error instead of the ECONNRESET error
     // when the connection is broken in some cases on MacOS and Linux
-    const isMacOrLinux = os.type() === 'Darwin' || os.type() === 'Linux';
+    const isMacOrLinux = osFamily.mac || osFamily.linux;
 
     if (err.code === 'ECONNRESET' || isMacOrLinux && err.code === 'EPIPE')
         return;
