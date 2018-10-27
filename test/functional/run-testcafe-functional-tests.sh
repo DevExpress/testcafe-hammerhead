bd4dee423ab15db21893fd8e80ec6f322624b984
Mikhail Losev <mlosev86@gmail.com>
2018-10-27 09:52:31 +0300

    Update browsers (#1801)
    * update browsers
    
    * small changes
    
    * fix remark
    
    * fix tests
    
    * update gulp task name

--- test/functional/run-testcafe-functional-tests.sh
+++ test/functional/run-testcafe-functional-tests.sh
@@ -7,5 +7,5 @@ git clone https://github.com/DevExpress/testcafe .
 npm install testcafe-hammerhead ../testcafe-hammerhead --save
 npm i --loglevel error
 
-export GULP_TASK="test-functional-local-headless"
+export GULP_TASK="test-functional-local-headless-chrome"
 npm test
