ba7c36b006bebddaecf0a3b26a3ef288558d1778
Artem Lavrov <artjomlav@gmail.com>
2018-10-30 15:25:06 +0300

    Upload folder is created if it does not exist and files with the same name are stored with the index (close #1797) (#1803)
    * Upload folder is created if it does not exist and files with the same name are stored with the index (close #1797)
    
    * fix tests
    
    * static copy and ensureUploadsRoot
    
    * small remarks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import Proxy from './proxy';
 import Session from './session';
 import ResponseMock from './request-pipeline/request-hooks/response-mock';
 import RequestFilterRule from './request-pipeline/request-hooks/request-filter-rule';
+import UploadStorage from './upload/storage';
 import { processScript, isScriptProcessed } from './processing/script';
 import ConfigureResponseEventOptions from './session/events/configure-response-event-options';
 import SAME_ORIGIN_CHECK_FAILED_STATUS_CODE from './request-pipeline/xhr/same-origin-check-failed-status-code';
@@ -9,6 +10,7 @@ import SAME_ORIGIN_CHECK_FAILED_STATUS_CODE from './request-pipeline/xhr/same-or
 export default {
     Proxy,
     Session,
+    UploadStorage,
     processScript,
     isScriptProcessed,
     ResponseMock,
--- src/upload/storage.js
+++ src/upload/storage.js
@@ -1,69 +1,150 @@
 import fs from 'fs';
 import mime from 'mime';
 import path from 'path';
+import { format } from 'util';
 import promisify from '../utils/promisify';
 
 const readFile  = promisify(fs.readFile);
 const stat      = promisify(fs.stat);
+const readDir   = promisify(fs.readdir);
+const makeDir   = promisify(fs.mkdir);
 const writeFile = promisify(fs.writeFile);
+const exists    = fsPath => stat(fsPath).then(() => true, () => false);
 
 export default class UploadStorage {
     constructor (uploadsRoot) {
         this.uploadsRoot = uploadsRoot;
     }
 
-    static async _loadFile (filePath) {
-        const fileContent = await readFile(filePath);
-        const stats       = await stat(filePath);
+    static async _getFilesToCopy (files) {
+        const filesToCopy = [];
+        const errs        = [];
 
-        return {
-            data: fileContent.toString('base64'),
-            info: {
-                lastModifiedDate: stats.mtime,
-                name:             path.basename(filePath),
-                type:             mime.lookup(filePath)
+        for (const file of files) {
+            try {
+                const stats = await stat(file.path);
+
+                if (stats.isFile())
+                    filesToCopy.push(file);
             }
-        };
+            catch (err) {
+                errs.push({ path: file.path, err });
+            }
+        }
+
+        return { filesToCopy, errs };
     }
 
-    async _runFileProcessingTask (fileName, processor) {
-        const resolvedPath = path.resolve(this.uploadsRoot, fileName);
+    static _generateName (existingNames, fileName) {
+        const extName  = path.extname(fileName);
+        const template = path.basename(fileName, extName) + ' %s' + extName;
+        let index      = 0;
 
+        while (existingNames.includes(fileName))
+            fileName = format(template, ++index);
+
+        return fileName;
+    }
+
+    static async _getExistingFiles (uploadsRoot) {
         try {
-            return await processor(resolvedPath, fileName);
+            return await readDir(uploadsRoot);
         }
         catch (e) {
-            return {
-                err:  e.toString(),
-                path: resolvedPath,
-                file: fileName
-            };
+            return [];
         }
-    }
-
-    async _processFiles (fileNames, processor) {
-        const processTasks = fileNames.map(fileName => this._runFileProcessingTask(fileName, processor));
-        let result         = await Promise.all(processTasks);
-
-        result = result.filter(value => !!value);
-
-        return result.length ? result : null;
     }
 
     async store (fileNames, data) {
-        return await this._processFiles(fileNames, async (resolvedPath, fileName) => {
-            const content = Buffer.from(data[fileNames.indexOf(fileName)], 'base64');
+        const storedFiles = [];
+        const err         = await UploadStorage.ensureUploadsRoot(this.uploadsRoot);
 
-            await writeFile(resolvedPath, content, { flag: 'wx' });
+        if (err)
+            return [{ err: err.toString(), path: this.uploadsRoot }];
 
-            return {
-                path: resolvedPath,
-                file: fileName
-            };
-        });
+        const existingFiles = await UploadStorage._getExistingFiles(this.uploadsRoot);
+
+        for (const fileName of fileNames) {
+            const storedFileName = UploadStorage._generateName(existingFiles, fileName);
+            const storedFilePath = path.join(this.uploadsRoot, storedFileName);
+
+            try {
+                await writeFile(storedFilePath, data[storedFiles.length], { encoding: 'base64' });
+
+                existingFiles.push(storedFileName);
+                storedFiles.push({ path: storedFilePath, file: storedFileName });
+            }
+            catch (e) {
+                storedFiles.push({ err: e.toString(), path: storedFilePath, file: fileName });
+            }
+        }
+
+        return storedFiles;
     }
 
-    async get (paths) {
-        return await this._processFiles(paths, async resolvedPath => await UploadStorage._loadFile(resolvedPath));
+    async get (filePathList) {
+        const result = [];
+
+        for (const filePath of filePathList) {
+            const resolvedPath = path.resolve(this.uploadsRoot, filePath);
+
+            try {
+                const fileContent = await readFile(resolvedPath);
+                const fileStats   = await stat(resolvedPath);
+
+                result.push({
+                    data: fileContent.toString('base64'),
+                    info: {
+                        lastModifiedDate: fileStats.mtime,
+                        name:             path.basename(resolvedPath),
+                        type:             mime.lookup(resolvedPath)
+                    }
+                });
+            }
+            catch (e) {
+                result.push({ err: e.toString(), path: filePath, resolvedPath });
+            }
+        }
+
+        return result;
+    }
+
+    static async copy (uploadsRoot, files) {
+        const { filesToCopy, errs } = await UploadStorage._getFilesToCopy(files);
+        const copiedFiles           = [];
+
+        if (!filesToCopy.length)
+            return { copiedFiles, errs };
+
+        const existingFiles = await UploadStorage._getExistingFiles(uploadsRoot);
+
+        for (const file of filesToCopy) {
+            const copiedFileName = UploadStorage._generateName(existingFiles, file.name);
+            const copiedFilePath = path.join(uploadsRoot, copiedFileName);
+
+            try {
+                await writeFile(copiedFilePath, await readFile(file.path, null));
+
+                existingFiles.push(copiedFileName);
+                copiedFiles.push(copiedFilePath);
+            }
+            catch (err) {
+                errs.push({ path: file.path, err });
+            }
+        }
+
+        return { copiedFiles, errs };
+    }
+
+    static async ensureUploadsRoot (uploadsRoot) {
+        try {
+            if (!await exists(uploadsRoot))
+                await makeDir(uploadsRoot);
+
+            return null;
+        }
+        catch (err) {
+            return err;
+        }
     }
 }
