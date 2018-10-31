29c9a667a6a885e2b8b3b099f61f68d987376800
LavrovArtem <artjomlav@gmail.com>
2018-10-31 18:28:58 +0300

    fix `Error: write ECONNABORTED` (close #1744)

--- src/request-pipeline/connection-reset-guard.js
+++ src/request-pipeline/connection-reset-guard.js
@@ -6,7 +6,8 @@ const connectionResetDomain = domain.create();
 connectionResetDomain.on('error', err => {
     // NOTE: The nodejs throw the EPIPE error instead of the ECONNRESET error
     // when the connection is broken in some cases on MacOS and Linux
-    if (err.code === 'ECONNRESET' || !os.win && err.code === 'EPIPE')
+    // https://github.com/nodejs/node/blob/8b4af64f50c5e41ce0155716f294c24ccdecad03/test/parallel/test-http-destroyed-socket-write2.js
+    if (err.code === 'ECONNRESET' || !os.win && err.code === 'EPIPE' || os.win && err.code === 'ECONNABORTED')
         return;
 
     connectionResetDomain.removeAllListeners('error');
