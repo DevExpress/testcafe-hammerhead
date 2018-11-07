58cb127d10adde9c9a70a99d0c897887587c2b62
miherlosev@mail.ru <Ein7Onai>
2018-11-07 17:54:05 +0300

    event emitter changes

--- src/client/utils/event-emitter.js
+++ src/client/utils/event-emitter.js
@@ -2,27 +2,27 @@ import nativeMethods from '../sandbox/native-methods';
 
 export default class EventEmitter {
     constructor () {
-        this.eventsListeners = [];
+        this.eventsListeners = nativeMethods.objectCreate(null);
     }
 
     emit (evt) {
         const listeners = this.eventsListeners[evt];
 
-        if (listeners) {
-            for (let i = 0; i < listeners.length; i++) {
-                try {
-                    if (listeners[i])
-                        listeners[i].apply(this, nativeMethods.arraySlice.apply(arguments, [1]));
-                }
-                catch (e) {
-                    // HACK: For IE: after calling document.write, the IFrameSandbox event handler throws the
-                    // 'Can't execute code from a freed script' exception because the document has been
-                    // recreated.
-                    if (e.message && e.message.indexOf('freed script') > -1)
-                        listeners[i] = null;
-                    else
-                        throw e;
-                }
+        if (!listeners)
+            return;
+
+        for (let i = 0; i < listeners.length; i++) {
+            try {
+                listeners[i].apply(this, nativeMethods.arraySlice.apply(arguments, [1]));
+            }
+            catch (e) {
+                // HACK: For IE: after calling document.write, the IFrameSandbox event handler throws the
+                // 'Can't execute code from a freed script' exception because the document has been
+                // recreated.
+                if (e.message && e.message.indexOf('freed script') > -1)
+                    listeners[i] = null;
+                else
+                    throw e;
             }
         }
     }
@@ -30,23 +30,17 @@ export default class EventEmitter {
     off (evt, listener) {
         const listeners = this.eventsListeners[evt];
 
-        if (listeners) {
-            const filtered = [];
+        if (!listeners)
+            return;
 
-            for (const currentListener of listeners) {
-                if (currentListener !== listener)
-                    filtered.push(currentListener);
-            }
-
-            this.eventsListeners[evt] = filtered;
-        }
+        this.eventsListeners[evt] = nativeMethods.arrayFilter.call(listeners, currentListener => currentListener !== listener);
     }
 
     on (evt, listener) {
-        if (!this.eventsListeners[evt])
-            this.eventsListeners[evt] = [];
+        this.eventsListeners[evt] = this.eventsListeners[evt] || [];
 
-        this.eventsListeners[evt].push(listener);
+        if (this.eventsListeners[evt].indexOf(listener) === -1)
+            this.eventsListeners[evt].push(listener);
 
         return listener;
     }
