5b2cbcc2760bd34696bdc6c13e73a3c0ebca369e
miherlosev@mail.ru <Ein7Onai>
2018-11-09 14:24:29 +0300

    add remark

--- test/client/fixtures/sandbox/iframe-test.js
+++ test/client/fixtures/sandbox/iframe-test.js
@@ -354,38 +354,41 @@ test('should not throw an exception if `Array.prototype.filter` was overriden (G
         });
 });
 
-test('should not dublicate internal handlers after `Sandbox.reattach` call', function () {
-    return createTestIframe()
-        .then(function (iframe) {
-            var checkDublicatedEventListeners = function (sandbox) {
-                var eventListeners = sandbox.eventsListeners;
+// NOTE: https://github.com/DevExpress/testcafe-hammerhead/issues/1821
+if (!browserUtils.isFirefox) {
+    test('should not dublicate internal handlers after `Sandbox.reattach` call', function () {
+        return createTestIframe()
+            .then(function (iframe) {
+                var checkDublicatedEventListeners = function (sandbox) {
+                    var eventListeners = sandbox.eventsListeners;
 
-                Object.keys(eventListeners).forEach(event => {
-                    var listenersArr = eventListeners[event].map(function (item) {
-                        return item.toString();
+                    Object.keys(eventListeners).forEach(event => {
+                        var listenersArr = eventListeners[event].map(function (item) {
+                            return item.toString();
+                        });
+
+                        var uniqueListenerArr = listenersArr.filter(function (element, index, arr) {
+                            return arr.indexOf(element) === index;
+                        });
+
+                        strictEqual(listenersArr.length, uniqueListenerArr.length);
                     });
+                };
 
-                    var uniqueListenerArr = listenersArr.filter(function (element, index, arr) {
-                        return arr.indexOf(element) === index;
-                    });
+                iframe.contentDocument.open();
+                iframe.contentDocument.write('Hello!');
+                iframe.contentDocument.close();
 
-                    strictEqual(listenersArr.length, uniqueListenerArr.length);
-                });
-            };
+                var iframeHammerhead = iframe.contentWindow['%hammerhead%'];
 
-            iframe.contentDocument.open();
-            iframe.contentDocument.write('Hello!');
-            iframe.contentDocument.close();
+                var testedSandboxes = [
+                    iframeHammerhead.shadowUI.iframeSandbox,
+                    iframeHammerhead.shadowUI.nodeMutation,
+                    iframeHammerhead.sandbox.codeInstrumentation.locationAccessorsInstrumentation
+                ];
 
-            var iframeHammerhead = iframe.contentWindow['%hammerhead%'];
-
-            var testedSandboxes = [
-                iframeHammerhead.shadowUI.iframeSandbox,
-                iframeHammerhead.shadowUI.nodeMutation,
-                iframeHammerhead.sandbox.codeInstrumentation.locationAccessorsInstrumentation
-            ];
-
-            testedSandboxes.forEach(sandbox => checkDublicatedEventListeners(sandbox));
-        });
-});
+                testedSandboxes.forEach(sandbox => checkDublicatedEventListeners(sandbox));
+            });
+    });
+}
 
