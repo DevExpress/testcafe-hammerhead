0d4cab0522a9fbedc0e4fc6447fd13295e03d980
Vladimir Airikh <vladimir.airich@gmail.com>
2018-11-19 11:21:38 +0300

    restore Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -45,48 +45,48 @@ const getClientTestSettings = () => {
 };
 
 const CLIENT_TESTS_BROWSERS = [
-    // {
-    //     platform:    'Windows 10',
-    //     browserName: 'MicrosoftEdge'
-    // },
-    // {
-    //     platform:    'Windows 10',
-    //     browserName: 'chrome'
-    // },
-    // {
-    //     platform:    'Windows 10',
-    //     browserName: 'firefox'
-    // },
+    {
+        platform:    'Windows 10',
+        browserName: 'MicrosoftEdge'
+    },
+    {
+        platform:    'Windows 10',
+        browserName: 'chrome'
+    },
+    {
+        platform:    'Windows 10',
+        browserName: 'firefox'
+    },
     {
         platform:    'Windows 8.1',
         browserName: 'internet explorer',
         version:     '11.0'
+    },
+    {
+        browserName: 'safari',
+        platform:    'macOS 10.13',
+        version:     '12.0'
+    },
+    {
+        browserName:     'Safari',
+        deviceName:      'iPhone 7 Plus Simulator',
+        platformVersion: '11.3',
+        platformName:    'iOS'
+    },
+    {
+        deviceName:      'Android GoogleAPI Emulator',
+        browserName:     'Chrome',
+        platformVersion: '7.1',
+        platformName:    'Android'
+    },
+    {
+        browserName: 'chrome',
+        platform:    'OS X 10.11'
+    },
+    {
+        browserName: 'firefox',
+        platform:    'OS X 10.11'
     }
-    // {
-    //     browserName: 'safari',
-    //     platform:    'macOS 10.13',
-    //     version:     '12.0'
-    // },
-    // {
-    //     browserName:     'Safari',
-    //     deviceName:      'iPhone 7 Plus Simulator',
-    //     platformVersion: '11.3',
-    //     platformName:    'iOS'
-    // },
-    // {
-    //     deviceName:      'Android GoogleAPI Emulator',
-    //     browserName:     'Chrome',
-    //     platformVersion: '7.1',
-    //     platformName:    'Android'
-    // },
-    // {
-    //     browserName: 'chrome',
-    //     platform:    'OS X 10.11'
-    // },
-    // {
-    //     browserName: 'firefox',
-    //     platform:    'OS X 10.11'
-    // }
 ];
 
 const SAUCELABS_SETTINGS = {
