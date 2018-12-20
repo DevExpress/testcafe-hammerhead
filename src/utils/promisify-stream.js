cfb25d3c9562349a4b79aeb6bba98c0246813926
miherlosev@mail.ru <Ein7Onai>
2018-12-20 15:15:14 +0300

    remove js files - 3

--- src/utils/promisify-stream.js
+++
@@ -1,9 +0,0 @@
-export default function (stream) {
-    return new Promise((resolve, reject) => {
-        const chunks = [];
-
-        stream.on('data', chunk => chunks.push(chunk));
-        stream.on('end', () => resolve(Buffer.concat(chunks)));
-        stream.on('error', error => reject(error));
-    });
-}
