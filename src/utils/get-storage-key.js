537aec6b5f94acf2188922f13ddc550ad458093a
miherlosev@mail.ru <Ein7Onai>
2018-12-20 15:07:19 +0300

    remove js files 2

--- src/utils/get-storage-key.js
+++
@@ -1,9 +0,0 @@
-// -------------------------------------------------------------
-// WARNING: this file is used by both the client and the server.
-// Do not use any browser or node-specific API!
-// -------------------------------------------------------------
-/* eslint hammerhead/proto-methods: 2 */
-const STORAGE_WRAPPER_PREFIX = 'hammerhead|storage-wrapper|';
-export default function getStorageKey(sessionId, host) {
-    return STORAGE_WRAPPER_PREFIX + sessionId + '|' + host;
-}
