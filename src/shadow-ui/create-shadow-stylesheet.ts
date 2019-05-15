3cea83824cc0b4099dca586cf9192a5331501817
Artem Lavrov <artjomlav@gmail.com>
2019-05-15 12:32:36 +0300

    Strict  typings for src/shadow-ui/create-shadow-stylesheet.ts (#2019)

--- src/shadow-ui/create-shadow-stylesheet.ts
+++ src/shadow-ui/create-shadow-stylesheet.ts
@@ -1,4 +1,6 @@
-import css from 'css';
+/*eslint-disable no-unused-vars*/
+import { default as css, Rule, Comment, AtRule, Document, Host, Media, Supports } from 'css';
+/*eslint-enable no-unused-vars*/
 import SHADOW_UI_CLASS_NAME from './class-name';
 
 const ID_OR_CLASS_RE: RegExp          = /#[a-zA-Z0-9_-]+|\.-?[a-zA-Z0-9_][a-zA-Z0-9_-]*/g;
@@ -8,27 +10,26 @@ function transformSelector (selector: string): string {
     return selector.replace(ID_OR_CLASS_RE, ADD_POSTFIX_REPLACEMENT);
 }
 
-function addUIClassPostfix (rules) {
-    let ruleStack = rules.slice();
+function addUIClassPostfix (rules: Array<Rule|Comment|AtRule>) {
+    for (const node of rules) {
+        if (node.type === 'rule') {
+            const rule = <Rule>node;
 
-    while (ruleStack.length) {
-        const rule = ruleStack.pop();
+            rule.selectors = rule.selectors && rule.selectors.map(transformSelector);
+        }
 
-        if (rule.type === 'rule' && rule.selectors)
-            rule.selectors = rule.selectors.map(transformSelector);
+        const nodeWithRules = <Document|Host|Media|Supports>node;
 
-        if (rule.rules)
-            ruleStack = ruleStack.concat(rule.rules);
+        if (nodeWithRules.rules)
+            addUIClassPostfix(nodeWithRules.rules);
     }
 }
 
 export default function createShadowStylesheet (cssCode: string) {
     const ast = css.parse(cssCode, { silent: true });
 
-    addUIClassPostfix(ast.stylesheet.rules);
+    if (ast.stylesheet)
+        addUIClassPostfix(ast.stylesheet.rules);
 
-    return css.stringify(ast, {
-        indent:   0,
-        compress: false
-    });
+    return css.stringify(ast, { compress: false });
 }
