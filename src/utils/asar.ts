71e20e6ccda686db17cffa9e03ffaf14684b0a5b
Vladimir Airikh <vladimir.airich@gmail.com>
2019-06-19 15:34:48 +0300

    update ASAR_ARCHIVE_PATH regex

--- src/utils/asar.ts
+++ src/utils/asar.ts
@@ -3,7 +3,7 @@ import asar from 'asar';
 import { toReadableStream } from './buffer';
 import { Readable } from 'stream'; // eslint-disable-line no-unused-vars
 
-const ASAR_ARCHIVE_PATH = /^.*\.asar/;
+const ASAR_ARCHIVE_PATH = /(^.+?\.asar)(?:\/)/;
 
 export function isAsarPath (fullPath: string) : boolean {
     return ASAR_ARCHIVE_PATH.test(fullPath);
@@ -12,12 +12,12 @@ export function isAsarPath (fullPath: string) : boolean {
 export function getArchivePath (fullPath: string) : string {
     const match = fullPath.match(ASAR_ARCHIVE_PATH);
 
-    return match ? match[0] : '';
+    return match ? match[1] : '';
 }
 
 export function extractFileToReadStream (fullPath: string) : Readable {
     const archive       = getArchivePath(fullPath);
-    const fileName      = fullPath.replace(ASAR_ARCHIVE_PATH, '.');
+    const fileName      = fullPath.replace(ASAR_ARCHIVE_PATH, './');
     const extractedFile = asar.extractFile(archive, fileName);
 
     return toReadableStream(extractedFile);
