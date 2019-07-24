9bd15d68d65a70ffa5159fe45263dcf90bd9c5a8
Vladimir Airikh <vladimir.airich@gmail.com>
2019-07-24 09:44:38 +0300

    add a file exist-check function

--- src/request-pipeline/file-request/create-resource.ts
+++ src/request-pipeline/file-request/create-resource.ts
@@ -4,13 +4,17 @@ import BaseResource from './base-resource';
 import FileSystemResource from './filesystem-resource';
 import AsarResource from './asar-resource';
 
+function isFileNotExists (errCode: string): boolean {
+    return errCode === 'ENOENT' ||
+        errCode === 'ENOTDIR'; // NOTE: found it (ENOTDIR) on travis server tests (GH-2043 PR)
+}
+
 export default async function createResource (path: string) : Promise<BaseResource> {
     let resource: BaseResource = new FileSystemResource(path);
 
     await resource.init();
 
-    if (resource.error &&
-        (resource.error.code === 'ENOENT' || resource.error.code === 'ENOTDIR')) {
+    if (resource.error && isFileNotExists(resource.error.code)) {
         const asarResource = new AsarResource(path);
 
         await asarResource.init();
