4ab1e60cb1efc0bcec105090b327dc6157a7c29b
Mikhail Losev <mlosev86@gmail.com>
2019-08-30 14:16:58 +0300

    use native util.promisify (#2123)
    * use native util.promisify
    
    * remove pify

--- src/utils/promisified-functions.ts
+++ src/utils/promisified-functions.ts
@@ -1,15 +1,14 @@
 import zlib from 'zlib';
-import promisify from './promisify';
+import { promisify } from 'util';
 import fs from 'fs';
 import childProcess from 'child_process';
 
-export const gzip       = promisify(zlib.gzip);
-export const deflate    = promisify(zlib.deflate);
-export const gunzip     = promisify(zlib.gunzip);
-export const inflate    = promisify(zlib.inflate);
-export const inflateRaw = promisify(zlib.inflateRaw);
-
-export const readDir: (path: string) => Promise<Array<string>> = promisify(fs.readdir);
+export const gzip: (buf: zlib.InputType, options?: zlib.ZlibOptions) => Promise<Buffer>       = promisify(zlib.gzip);
+export const deflate: (buf: zlib.InputType, options?: zlib.ZlibOptions) => Promise<Buffer>    = promisify(zlib.deflate);
+export const gunzip: (buf: zlib.InputType, options?: zlib.ZlibOptions) => Promise<Buffer>     = promisify(zlib.gunzip);
+export const inflate: (buf: zlib.InputType, options?: zlib.ZlibOptions) => Promise<Buffer>    = promisify(zlib.inflate);
+export const inflateRaw: (buf: zlib.InputType, options?: zlib.ZlibOptions) => Promise<Buffer> = promisify(zlib.inflateRaw);
+export const readDir: (path: string) => Promise<Array<string>>                                = promisify(fs.readdir);
 
 export const readFile       = promisify(fs.readFile);
 export const stat           = promisify(fs.stat);
--- src/utils/promisify.ts
+++
@@ -1,6 +0,0 @@
-// @ts-ignore
-import pify from 'pify';
-
-export default function (fn: Function) {
-    return pify(fn, Promise);
-}
