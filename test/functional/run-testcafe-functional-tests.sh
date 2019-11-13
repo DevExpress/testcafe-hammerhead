5b36bd32b3f0b48fe68ef163c2052b7b4ec22b68
LavrovArtem <artjomlav@gmail.com>
2019-11-13 14:00:22 +0300

    try to fix functional tests

--- test/functional/run-testcafe-functional-tests.sh
+++ test/functional/run-testcafe-functional-tests.sh
@@ -5,6 +5,8 @@ cd ../testcafe
 git clone https://github.com/DevExpress/testcafe .
 
 npm install testcafe-hammerhead ../testcafe-hammerhead --save
+npm i
+npm run prepare
 npm i --loglevel error
 
 export GULP_TASK="test-functional-local-headless-chrome"
