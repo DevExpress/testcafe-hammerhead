b421e54f511363768293dda458f2d8389226fc37
Andrey Belym <belym.a.2105@gmail.com>
2019-11-13 15:11:57 +0300

    Fix the tests

--- test/functional/run-testcafe-functional-tests.sh
+++ test/functional/run-testcafe-functional-tests.sh
@@ -4,10 +4,11 @@ mkdir ../testcafe
 cd ../testcafe
 git clone https://github.com/DevExpress/testcafe .
 
-npm install testcafe-hammerhead ../testcafe-hammerhead --save
-npm i
-npm run prepare
-npm i --loglevel error
-
+# NOTE: testcafe's Gulpfile.js tries to alias the 'travis' task to the value of GULP_TASK
+# We should define a valid testcafe's task in GULP_TASK before running 'npm install',
+# which automatically builds testcafe with Gulp. 
 export GULP_TASK="test-functional-local-headless-chrome"
+
+npm install testcafe-hammerhead ../testcafe-hammerhead --save
+
 npm test
