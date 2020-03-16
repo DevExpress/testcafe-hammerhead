f8a33a9d8be889d018efb7b1d67f441be80b470f
Artem Lavrov <artjomlav@gmail.com>
2020-03-16 10:38:22 +0300

    fix `onResponse not called for AJAX requests when hook constructor receives responseEventConfigureOpts` (close #2190)

--- src/session/events/configure-response-event-options.ts
+++ src/session/events/configure-response-event-options.ts
@@ -1,10 +1,21 @@
 export default class ConfigureResponseEventOptions {
-    readonly includeHeaders: boolean;
-    readonly includeBody: boolean;
+    constructor (private _includeHeaders: boolean = false, private _includeBody: boolean = false) {
+    }
 
-    constructor (includeHeaders: boolean = false, includeBody: boolean = false) {
-        this.includeHeaders = includeHeaders;
-        this.includeBody    = includeBody;
+    get includeHeaders () {
+        return this._includeHeaders;
+    }
+
+    set includeHeaders (value: any) {
+        this._includeHeaders = !!value;
+    }
+
+    get includeBody () {
+        return this._includeBody;
+    }
+
+    set includeBody (value: any) {
+        this._includeBody = !!value;
     }
 
     static get DEFAULT (): ConfigureResponseEventOptions {
