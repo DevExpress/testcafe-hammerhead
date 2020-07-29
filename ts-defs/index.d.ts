2971bab47a22133871c7bec1af01557af3438459
miherlosev <miherlosev@mail.ru>
2020-07-29 17:30:55 +0300

    More typing for hammerhead

--- ts-defs/index.d.ts
+++ ts-defs/index.d.ts
@@ -17,7 +17,7 @@ interface Session {
 }
 
 declare module 'testcafe-hammerhead' {
-    /** The Proxy class used to create a web-proxy **/
+    /** The Proxy class is used to create a web-proxy **/
     export class Proxy {
         /** Creates a web proxy instance **/
         constructor ();
@@ -41,6 +41,30 @@ declare module 'testcafe-hammerhead' {
         unRegisterRoute (route: string, method: string): void;
     }
 
+    /** The RequestFilterRule class is used to create URL filtering rules for request hook **/
+    export class RequestFilterRule {
+        /** Creates a request filter rule instance **/
+        constructor (options: any);
+
+        /** Returns the value that accepts any request  **/
+        static ANY: RequestFilterRule;
+
+        /** Check whether the specified RequestFilterRule instance accepts any request **/
+        static isANY (instance: any): boolean;
+    }
+
     /** Generates an URL friendly string identifier **/
     export function generateUniqueId(length?: number): string;
+
+    /** Inject into specified text the service scripts instructions **/
+    export function processScript(src: string, withHeader?: boolean, wrapLastExprWithProcessHtml?: boolean, resolver?: Function): string;
+
+    /** Check whether specified code contains the service script instructions **/
+    export function isScriptProcessed (code: string): boolean;
+
+    /** The URL of the service blank page **/
+    export const SPECIAL_BLANK_PAGE: string;
+
+    /** The URL of the service error page **/
+    export const SPECIAL_ERROR_PAGE: string;
 }
