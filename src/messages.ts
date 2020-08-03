ff1790f279581b52a32828f9cf1dedfd7d185be3
Dmitrii <ddm157@yandex.ru>
2020-08-03 09:34:38 +0300

    Added error messages (still WIP)

--- src/messages.ts
+++ src/messages.ts
@@ -7,8 +7,8 @@ export const MESSAGE = {
     destRequestTimeout:               'Failed to complete a request to <a href="{url}">{url}</a> within the timeout period. The problem may be related to local machine\'s network or firewall settings, server outage, or network problems that make the server inaccessible.',
     cantReadFile:                     'Failed to read a file at <a href="{url}">{url}</a> because of the error:\n\n{message}',
     nodeError: {
-        'HPE_HEADER_OVERFLOW': 'header overflow error message',
-        'ERR_INVALID_CHAR':    'invalid char error message'
+        'HPE_HEADER_OVERFLOW': 'The request header\'s size exceeds the set limit.\nIt causes an internal Node.js error on parsing this header.\n To fix the problem, you need to specify the maximum header size via the NODE_OPTIONS=\'--max-http-header-size=...\' environment variable.',
+        'ERR_INVALID_CHAR':    'The request contains a header that doesn\'t comply with the specification <a href="https://httpwg.org/specs/rfc7230.html#rfc.section.3.2">https://httpwg.org/specs/rfc7230.html#rfc.section.3.2</a>.\nIt causes an internal Node.js error on parsing this header.\n To fix the problem, you need to specify the legacy http header parser via the NODE_OPTIONS=\'--insecure-http-parser\' environment variable or change the header name according to the specification.'
     }
 };
 
