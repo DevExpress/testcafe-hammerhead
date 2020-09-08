b1e63a66dbb8f22972fbaf5017c749176754fbda
Mikhail Losev <mlosev86@gmail.com>
2020-09-08 11:14:19 +0300

    Add more typing (#2431)
    * add more typing
    
    * bump version
    
    * Move import statement

--- ts-defs/index.d.ts
+++ ts-defs/index.d.ts
@@ -17,6 +17,8 @@ interface Session {
 }
 
 declare module 'testcafe-hammerhead' {
+    import { IncomingHttpHeaders } from 'http';
+
     /** The Proxy class is used to create a web-proxy **/
     export class Proxy {
         /** Creates a web proxy instance **/
@@ -59,6 +61,84 @@ declare module 'testcafe-hammerhead' {
         static empty (): StateSnapshot;
     }
 
+    /** The ConfigureResponseEventOptions contains options to set up ResponseEvent **/
+    export class ConfigureResponseEventOptions {
+        /** Creates an instance of ConfigureResponseEventOptions **/
+        constructor(includeHeaders: boolean, includeBody: boolean);
+
+        /** Specified whether to include headers to ResponseEvent **/
+        includeHeaders: boolean;
+
+        /** Specified whether to include body to Response **/
+        includeBody: boolean;
+    }
+
+    /** The ConfigureResponseEvent is used to set up the ResponseEvent **/
+    export class ConfigureResponseEvent {
+        /** The options to configure ResponseEvent **/
+        opts: ConfigureResponseEventOptions;
+    }
+
+    /** The RequestInfo class contains information about query request **/
+    export class RequestInfo {
+        /** Request unique identifier **/
+        requestId: string;
+
+        /** Session unique identifier **/
+        sessionId: string;
+
+        /** The user agent of the query request **/
+        userAgent: string;
+
+        /** The url of the query request **/
+        url: string;
+
+        /** The method of the query request **/
+        method: string;
+
+        /** The headers of the query request **/
+        headers: IncomingHttpHeaders;
+
+        /** The body of the query request **/
+        body: Buffer;
+    }
+
+    /** The RequestEvent describes the request part of the query captured with request hook **/
+    export class RequestEvent {
+        /** The information of the query request **/
+        _requestInfo: RequestInfo;
+
+        /** The filter rule for the query **/
+        _requestFilterRule: RequestFilterRule;
+
+        /** Set up the mock for the query response **/
+        setMock(mock: ResponseMock): void;
+    }
+
+    /** The ResponseEvent describes the response part of the query captured with request hook **/
+    export class ResponseEvent {
+        /** The filter rule for the query **/
+        _requestFilterRule: RequestFilterRule;
+
+        /** Request unique identifier **/
+        requestId: string;
+
+        /** The status code of the query **/
+        statusCode: string;
+
+        /** The headers of the query response **/
+        headers: IncomingHttpHeaders;
+
+        /** The body of the query response **/
+        body: Buffer;
+    }
+
+    /** The ResponseMock class is used to construct the response of the mocked request **/
+    export class ResponseMock {
+        /** Creates a ResponseMock instance **/
+        constructor(body: string | Function, statusCode?: number, headers?: object);
+    }
+
     /** Generates an URL friendly string identifier **/
     export function generateUniqueId(length?: number): string;
 
@@ -73,4 +153,7 @@ declare module 'testcafe-hammerhead' {
 
     /** The URL of the service error page **/
     export const SPECIAL_ERROR_PAGE: string;
+
+    /** The internal status code indicating to failed same origin check **/
+    export const SAME_ORIGIN_CHECK_FAILED_STATUS_CODE: number;
 }
