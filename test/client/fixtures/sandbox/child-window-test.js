891c5497485ca999de285f794010d90cabe47f68
Artem Lavrov <artjomlav@gmail.com>
2020-11-18 08:47:16 +0300

    fix `Network requests aren't recieved by server after clicking on a link` (close #2467) (#2483)

--- test/client/fixtures/sandbox/child-window-test.js
+++ test/client/fixtures/sandbox/child-window-test.js
@@ -3,6 +3,7 @@ var defaultTarget      = hammerhead.get('./sandbox/child-window/default-target')
 var settings           = hammerhead.get('./settings');
 
 var windowSandbox = hammerhead.sandbox.node.win;
+var nativeMethods = hammerhead.nativeMethods;
 
 test('_shouldOpenInNewWindow', function () {
     window.name = 'test-window-name';
@@ -29,4 +30,39 @@ test('window.open', function () {
     window.open('/test');
 
     settings.get().allowMultipleWindows = false;
+});
+
+module('regression');
+
+test('should be prevented only default behaviour (GH-2467)', function () {
+    settings.get().allowMultipleWindows = true;
+
+    var anchor                   = document.createElement('a');
+    var isClickTriggered         = false;
+    var storedOpenUrlInNewWindow = windowSandbox._childWindowSandbox._openUrlInNewWindow;
+
+    anchor.href        = '/path/';
+    anchor.target      = '_blank';
+    anchor.textContent = 'click';
+
+    document.body.appendChild(anchor);
+
+    anchor.addEventListener('click', function () {
+        isClickTriggered = true;
+    });
+
+    windowSandbox._childWindowSandbox._openUrlInNewWindow = window.noop;
+
+    nativeMethods.click.call(anchor);
+
+    return window.QUnitGlobals.wait(function () {
+        return isClickTriggered;
+    }, 5000)
+        .then(function () {
+            ok(true);
+
+            settings.get().allowMultipleWindows = false;
+            windowSandbox._childWindowSandbox._openUrlInNewWindow = storedOpenUrlInNewWindow;
+            document.body.removeChild(anchor);
+        });
 });
