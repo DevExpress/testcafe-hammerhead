b40e6659fa2913614f9af3f73740104047fbb721
Pavel Redrukhin <wabloxa@yandex.ru>
2020-12-04 13:58:26 +0300

    add check in test that window has `require` method

--- test/client/fixtures/sandbox/electron-test.js
+++ test/client/fixtures/sandbox/electron-test.js
@@ -2,19 +2,22 @@ const nativeMethods = hammerhead.nativeMethods;
 const browserUtils  = hammerhead.utils.browser;
 
 if (browserUtils.isElectron) {
-    const vm = window.require('vm');
+    // NOTE: Electron window created without `nodeIntegration: true` doesn't have `window.require`
+    if (window.require) {
+        const vm = window.require('vm');
 
-    if (vm) {
-        test('wrappers of native functions should return the correct string representations', function () {
-            window.checkStringRepresentation(vm.createScript, nativeMethods.createScript, 'vm.createScript');
+        if (vm) {
+            test('wrappers of native functions should return the correct string representations', function () {
+                window.checkStringRepresentation(vm.createScript, nativeMethods.createScript, 'vm.createScript');
 
-            // NOTE: DebugContext has been removed in V8 and is not available in Node.js 10+
-            if (vm.runInDebugContext)
-                window.checkStringRepresentation(vm.runInDebugContext, nativeMethods.runInDebugContext, 'vm.runInDebugContext');
+                // NOTE: DebugContext has been removed in V8 and is not available in Node.js 10+
+                if (vm.runInDebugContext)
+                    window.checkStringRepresentation(vm.runInDebugContext, nativeMethods.runInDebugContext, 'vm.runInDebugContext');
 
-            window.checkStringRepresentation(vm.runInContext, nativeMethods.runInContext, 'vm.runInContext');
-            window.checkStringRepresentation(vm.runInNewContext, nativeMethods.runInNewContext, 'vm.runInNewContext');
-            window.checkStringRepresentation(vm.runInThisContext, nativeMethods.runInThisContext, 'vm.runInThisContext');
-        });
+                window.checkStringRepresentation(vm.runInContext, nativeMethods.runInContext, 'vm.runInContext');
+                window.checkStringRepresentation(vm.runInNewContext, nativeMethods.runInNewContext, 'vm.runInNewContext');
+                window.checkStringRepresentation(vm.runInThisContext, nativeMethods.runInThisContext, 'vm.runInThisContext');
+            });
+        }
     }
 }
