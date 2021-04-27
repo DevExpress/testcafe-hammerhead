dc5c9ae49a24b7a08498e0acb32123c46b4f8fb9
Andrey <andrey.belym@devexpress.com>
2021-04-27 11:38:57 +0300

    ci: fix mistype

--- Gulpfile.js
+++ Gulpfile.js
@@ -143,7 +143,7 @@ gulp.task('build',
     )
 );
 
-const BUILD_TASK = noBuild ? noop : gulp.registry.get('build');
+const BUILD_TASK = noBuild ? noop : gulp.registry()ß.get('build');
 
 // Test
 gulp.step('test-server-run', () => {
