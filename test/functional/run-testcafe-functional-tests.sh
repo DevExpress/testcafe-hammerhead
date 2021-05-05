f330f7122081159eb0131425735924cc03b3ffc0
Pavel Redrukhin <wabloxa@yandex.ru>
2021-05-05 19:57:58 +0300

    test the fix on testcafe side

--- test/functional/run-testcafe-functional-tests.sh
+++ test/functional/run-testcafe-functional-tests.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env bash
 
-# pwd is /home/runner/work/testcafe-hammerhead/testcafe-hammerhead
-mkdir ../../testcafe
-
-cd ../../testcafe
-# pwd is /home/runner/work/testcafe
-
-git clone https://github.com/DevExpress/testcafe .
-
-patch -p0 < ../testcafe-hammerhead/testcafe-hammerhead/patch_test.patch
+mkdir ../testcafe
+cd ../testcafe
+git clone https://github.com/wentwrong/testcafe --branch fix_stacktrace_hh_filtering .
 
 # NOTE: testcafe's Gulpfile.js tries to alias the 'travis' task to the value of GULP_TASK
 # We should define a valid testcafe's task in GULP_TASK before running 'npm install',
 # which automatically builds testcafe with Gulp. 
 export GULP_TASK="test-functional-local-headless-chrome"
 
-npm install testcafe-hammerhead ../testcafe-hammerhead/testcafe-hammerhead --save
+npm install testcafe-hammerhead ../testcafe-hammerhead --save
 npm i --loglevel error
 
-npx gulp fast-build --steps-as-tasks
-npx gulp test-functional-local-headless-chrome-run --steps-as-tasks
+npm test
