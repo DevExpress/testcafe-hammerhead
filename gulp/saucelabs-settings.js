ac217dbc4a30fb657950e73923a7ebbdb543763b
miherlosev <miherlosev@mail.ru>
2022-01-12 14:15:42 +0300

    3

--- gulp/saucelabs-settings.js
+++ gulp/saucelabs-settings.js
@@ -1,5 +1,5 @@
 const CLIENT_TESTS_BROWSERS = [
-    {
+   /* {
         platform:    'Windows 10',
         browserName: 'MicrosoftEdge'
     },
@@ -20,13 +20,13 @@ const CLIENT_TESTS_BROWSERS = [
         browserName: 'safari',
         platform:    'macOS 11.00',
         version:     '14'
-    },
+    },*/
     {
         browserName: 'safari',
         platform:    'macOS 12',
         version:     '15'
     },
-    {
+    /*{
         browserName:     'Safari',
         deviceName:      'iPhone 7 Plus Simulator',
         platformVersion: '11.3',
@@ -45,7 +45,7 @@ const CLIENT_TESTS_BROWSERS = [
     {
         browserName: 'firefox',
         platform:    'macOS 12'
-    }
+    }*/
 ];
 
 const SAUCELABS_SETTINGS = {
