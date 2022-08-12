72ee3efe4a2a76605bf1d39eb4c2ed91808e9a07
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-08-12 14:18:00 +0400

    test: added tests overrideDescriptor in prototype

--- test/client/fixtures/utils/overriding-test.js
+++ test/client/fixtures/utils/overriding-test.js
@@ -28,3 +28,32 @@ test('overrideDescriptor', function () {
     ok(overriddenDescriptor.configurable);
     ok(overriddenDescriptor.enumerable);
 });
+
+test('overrideDescriptor in prototype', function () {
+    var prototype = {};
+
+    var obj = {
+        _prop: 'value',
+    };
+
+    Object.setPrototypeOf(obj, prototype);
+
+    Object.defineProperty(prototype, 'prop', {
+        get: function () {
+            return this._prop;
+        },
+        set: void 0,
+
+        configurable: true,
+        enumerable:   true,
+    });
+
+    overriding.overrideDescriptor(obj, 'prop', {
+        getter: function () {
+            return 'overridden ' + this._prop;
+        },
+        setter: void 0,
+    });
+
+    strictEqual(obj.prop, 'overridden value');
+});
