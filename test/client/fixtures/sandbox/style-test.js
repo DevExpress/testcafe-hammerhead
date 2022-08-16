8339b9e73ceca94b22915bd0aa91d7714047d2f9
Aleksey Popov <alekseypopow1995@yandex.ru>
2022-08-16 15:12:31 +0400

    fix: processed comments

--- test/client/fixtures/sandbox/style-test.js
+++ test/client/fixtures/sandbox/style-test.js
@@ -192,8 +192,7 @@ test('override properties in the CSSStyleDeclaration.prototype (GH-7166)', funct
             var iframeDocument = iframe.contentDocument;
             var item           = iframeDocument.querySelector('div');
 
-            ok(item.style.backgroundImage);
-            ok(item.style['background-image']);
-            strictEqual(item.style.backgroundImage, item.style['background-image']);
+            strictEqual(item.style.backgroundImage, 'url("https://domain.com/test.svg")');
+            strictEqual(item.style.backgroundImage, 'url("https://domain.com/test.svg")');
         });
 });
