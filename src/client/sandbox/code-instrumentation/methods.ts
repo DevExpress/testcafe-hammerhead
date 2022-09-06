db6ce30c310dc32d9c8baf725dabef4b2ce7fc87
miherlosev <miherlosev@mail.ru>
2022-09-06 13:08:00 +0400

    fix tests

--- src/client/sandbox/code-instrumentation/methods.ts
+++ src/client/sandbox/code-instrumentation/methods.ts
@@ -28,24 +28,20 @@ export default class MethodCallInstrumentation extends SandboxBase {
         if (this.proxyless)
             return;
 
-        this.methodWrappers = {
-            // NOTE: We cannot get the location wrapper for a cross-domain window. Therefore, we need to
-            // intercept calls to the native 'replace' method.
-            replace: {
-                condition: isLocation,
-                method:    (location: Location, args: any[]) => location.replace(getProxyUrl(args[0], {
-                    resourceType: MethodCallInstrumentation._getLocationResourceType(location),
-                })),
-            },
+        // NOTE: We cannot get the location wrapper for a cross-domain window. Therefore, we need to
+        // intercept calls to the native 'replace' and 'assign' methods.
+        this.methodWrappers.replace = {
+            condition: isLocation,
+            method:    (location: Location, args: any[]) => location.replace(getProxyUrl(args[0], {
+                resourceType: MethodCallInstrumentation._getLocationResourceType(location),
+            })),
+        };
 
-            // NOTE: We cannot get the location wrapper for a cross-domain window. Therefore, we need to
-            // intercept calls to the native 'assign' method.
-            assign: {
-                condition: isLocation,
-                method:    (location: Location, args: any[]) => location.assign(getProxyUrl(args[0], {
-                    resourceType: MethodCallInstrumentation._getLocationResourceType(location),
-                })),
-            },
+        this.methodWrappers.assign = {
+            condition: isLocation,
+            method:    (location: Location, args: any[]) => location.assign(getProxyUrl(args[0], {
+                resourceType: MethodCallInstrumentation._getLocationResourceType(location),
+            })),
         };
     }
 
