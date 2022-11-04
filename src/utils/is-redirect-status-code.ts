d3952aaafb20bd6cd4a66a2c40c59bd78b8c1db8
miherlosev <miherlosev@mail.ru>
2022-11-04 15:31:23 +0400

    isRedirectStatusCode: make parameter optional

--- src/utils/is-redirect-status-code.ts
+++ src/utils/is-redirect-status-code.ts
@@ -1,5 +1,8 @@
 const REDIRECT_STATUS_CODES = [301, 302, 303, 307, 308];
 
-export default function (code: number): boolean {
+export default function (code?: number): boolean {
+    if (!code)
+        return false;
+
     return REDIRECT_STATUS_CODES.includes(code);
 }
