0400a2c5743554540987e7a92db88cb55f310be5
artem-babich <artem.babich@devexpress.com>
2023-02-09 11:32:29 +0400

    remove fork dependency

--- test/functional/run-testcafe-functional-tests.sh
+++ test/functional/run-testcafe-functional-tests.sh
@@ -2,7 +2,7 @@
 
 mkdir ../testcafe
 cd ../testcafe
-git clone --branch Babich-fix-request https://github.com/Artem-Babich/testcafe.git .
+git clone https://github.com/DevExpress/testcafe .
 
 # NOTE: testcafe's Gulpfile.js tries to alias the 'travis' task to the value of GULP_TASK
 # We should define a valid testcafe's task in GULP_TASK before running 'npm install',
