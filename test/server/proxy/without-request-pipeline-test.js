f20153fa1076fcf23a5494eaa20cd6f23e0d2adc
Alex Kamaev <alexikam@gmail.com>
2023-06-13 13:08:09 +0400

    add server test

--- test/server/proxy/without-request-pipeline-test.js
+++ test/server/proxy/without-request-pipeline-test.js
@@ -3,18 +3,27 @@ const { injectResources } = require('../../../lib');
 const { compareCode }     = require('../common/utils');
 
 describe('New API', () => {
+    const resources = {
+        stylesheets:     ['./styles.css'],
+        scripts:         ['common/script1.js', './common/script2.js'],
+        embeddedScripts: ['var script1 = 1;', 'var script2 = 2;'],
+        userScripts:     ['/custom-script1.js', '/custom-script2.js'],
+    };
+
+    const pageContent         = fs.readFileSync('test/server/data/without-request-pipeline/src.html').toString();
+    const expectedPageContent = fs.readFileSync('test/server/data/without-request-pipeline/expected.html').toString();
+
     it('PageProcessor.injectResources', () => {
-        const pageContent = fs.readFileSync('test/server/data/without-request-pipeline/src.html').toString();
+        const updatedPageContent = injectResources(pageContent, resources);
 
-        const resources = {
-            stylesheets:     ['./styles.css'],
-            scripts:         ['common/script1.js', './common/script2.js'],
-            embeddedScripts: ['var script1 = 1;', 'var script2 = 2;'],
-            userScripts:     ['/custom-script1.js', '/custom-script2.js'],
-        };
+        compareCode(updatedPageContent, expectedPageContent);
+    });
 
-        const updatedPageContent  = injectResources(pageContent, resources);
-        const expectedPageContent = fs.readFileSync('test/server/data/without-request-pipeline/expected.html').toString();
+    it('PageProcessor.injectResources - trim BOM', () => {
+        const bomSymbol          = String.fromCharCode(65279);
+        const updatedPageContent = injectResources(bomSymbol + pageContent, resources);
+
+        updatedPageContent.charCodeAt(0) === bomSymbol;
 
         compareCode(updatedPageContent, expectedPageContent);
     });
