90a04ae680d68e8d53eb3c2f5766bd7d83dae95f
Popov Aleksey <Alekseypopow1995@yandex.ru>
2023-07-17 14:55:50 +0300

    refactor: remove IE leftovers in typings

--- src/typings/extend-html-element.d.ts
+++
@@ -1,5 +0,0 @@
-// NOTE: This type is extended for compatibility with IE11.
-// see https://docs.microsoft.com/en-us/previous-versions/ff975201(v=vs.85)
-interface HTMLElement {
-    msMatchesSelector: Function;
-}
