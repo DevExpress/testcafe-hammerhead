95320cee3a5669ea198d9b676d621732c7fafc10
Pavel <palich_morozov@mail.ru>
2023-11-22 12:52:13 +0400

    fix test

--- test/client/fixtures/sandbox/event/unload-test.js
+++ test/client/fixtures/sandbox/event/unload-test.js
@@ -77,36 +77,6 @@ if (browserUtils.isSafari && !browserUtils.isIOS) {
     });
 }
 
-test('onbeforeunload handler must be called with and prevented native dialog with empty return value(GH-6815)', function () {
-    return createTestIframe({ src: getCrossDomainPageUrl('../../../data/unload/iframe-beforeunload.html') })
-        .then(function (iframe) {
-            postMessage(iframe.contentWindow, [{
-                state:       'reload',
-                returnValue: '',
-            }, '*']);
-
-            return waitForMessage(window);
-        })
-        .then(function (returnValue) {
-            ok(returnValue);
-        });
-});
-
-test('onbeforeunload handler must be called with and prevented native dialog with return value(GH-6815)', function () {
-    return createTestIframe({ src: getCrossDomainPageUrl('../../../data/unload/iframe-beforeunload.html') })
-        .then(function (iframe) {
-            postMessage(iframe.contentWindow, [{
-                state:       'reload',
-                returnValue: 'message',
-            }, '*']);
-
-            return waitForMessage(window);
-        })
-        .then(function (returnValue) {
-            ok(returnValue);
-        });
-});
-
 if (!browserUtils.isSafari) {
     test('Should save the returnValue as string', function () {
         return createTestIframe({ src: getCrossDomainPageUrl('../../../data/unload/iframe.html') })
@@ -121,6 +91,36 @@ if (!browserUtils.isSafari) {
             })
             .then(function (returnValue) {
                 strictEqual(returnValue, '[object BeforeUnloadEvent]');
+            });
+    });
+
+    test('onbeforeunload handler must be called with and prevented native dialog with empty return value(GH-6815)', function () {
+        return createTestIframe({ src: getCrossDomainPageUrl('../../../data/unload/iframe-beforeunload.html') })
+            .then(function (iframe) {
+                postMessage(iframe.contentWindow, [{
+                    state:       'reload',
+                    returnValue: '',
+                }, '*']);
+
+                return waitForMessage(window);
+            })
+            .then(function (returnValue) {
+                ok(returnValue);
+            });
+    });
+
+    test('onbeforeunload handler must be called with and prevented native dialog with return value(GH-6815)', function () {
+        return createTestIframe({ src: getCrossDomainPageUrl('../../../data/unload/iframe-beforeunload.html') })
+            .then(function (iframe) {
+                postMessage(iframe.contentWindow, [{
+                    state:       'reload',
+                    returnValue: 'message',
+                }, '*']);
+
+                return waitForMessage(window);
+            })
+            .then(function (returnValue) {
+                ok(returnValue);
             });
     });
 }
