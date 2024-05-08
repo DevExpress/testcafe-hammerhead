5e7a745e7a63b77efd8c02a820287dca988d8ff0
Bayheck <adil.rakhaliyev@devexpress.com>
2024-05-08 15:12:20 +0500

    fix tests

--- src/client/utils/position.ts
+++ src/client/utils/position.ts
@@ -209,7 +209,7 @@ export function getElementRectangle (el) {
         rectangle = getSelectChildRectangle(el);
     else {
         const elementOffset     = getOffsetPosition(el);
-        const relativeRectangle = domUtils.isSVGElementOrChild(el) ? getSvgElementRelativeRectangle(el) : el.getClientRects()[0];
+        const relativeRectangle = domUtils.isSVGElementOrChild(el) ? getSvgElementRelativeRectangle(el) : el.getClientRects()[0] ?? { height: 0, width: 0 };
 
         rectangle = {
             height: relativeRectangle.height,
--- src/client/utils/style.ts
+++ src/client/utils/style.ts
@@ -319,7 +319,7 @@ export function getOffset (el) {
     if (!el || domUtils.isWindow(el) || domUtils.isDocument(el))
         return null;
 
-    let clientRect = el.getClientRects()[0];
+    let clientRect = el.getClientRects()[0] ?? { top: 0, left: 0 };
 
     // NOTE: A detached node or documentElement.
     const doc        = el.ownerDocument;
@@ -338,7 +338,7 @@ export function getOffset (el) {
     const scrollTop  = win.pageYOffset || docElement.scrollTop || doc.body.scrollTop;
     const scrollLeft = win.pageXOffset || docElement.scrollLeft || doc.body.scrollLeft;
 
-    clientRect = el.getClientRects()[0];
+    clientRect = el.getClientRects()[0] ?? { top: 0, left: 0 };
 
     return {
         top:  clientRect.top + scrollTop - clientTop,
