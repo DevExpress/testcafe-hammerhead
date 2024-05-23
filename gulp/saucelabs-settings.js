471d910cd260e00b114c68d67b56dd0956b87a89
Bayheck <adil.rakhaliyev@devexpress.com>
2024-05-23 16:51:04 +0500

    isolate windows 10 chrome cloud tests

--- gulp/saucelabs-settings.js
+++ gulp/saucelabs-settings.js
@@ -1,37 +1,7 @@
 const CLIENT_TESTS_BROWSERS = [
     {
         platform:    'Windows 10',
-        browserName: 'MicrosoftEdge',
-    },
-    {
-        platform:    'Windows 10',
         browserName: 'chrome',
-    },
-    {
-        platform:    'Windows 10',
-        browserName: 'firefox',
-    },
-    {
-        browserName: 'safari',
-        platform:    'macOS 11.00',
-        version:     '14',
-    },
-    {
-        browserName: 'safari',
-        platform:    'macOS 12',
-        version:     '15',
-    },
-    {
-        browserName:     'Safari',
-        deviceName:      'iPhone 7 Plus Simulator',
-        platformVersion: '11.3',
-        platformName:    'iOS',
-    },
-    {
-        deviceName:      'Android GoogleAPI Emulator',
-        browserName:     'Chrome',
-        platformVersion: '7.1',
-        platformName:    'Android',
     },
 ];
 
