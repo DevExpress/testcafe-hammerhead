7f90389f82db8647dabce38c07509160af26d9ec
adil.rakhaliyev <adil.rakhaliyev@devexpress.com>
2026-01-12 20:38:26 +0500

    fix: clean code

--- src/processing/script/header.ts
+++ src/processing/script/header.ts
@@ -17,7 +17,6 @@ const STRICT_MODE_PLACEHOLDER     = '{strict-placeholder}';
 const SW_SCOPE_HEADER_VALUE       = '{sw-scope-header-value}';
 const WORKER_SETTINGS_PLACEHOLDER = '{worker-settings}';
 
-// NOTE: we need try/catch if a worker with ES module (fix for https://github.com/DevExpress/testcafe/issues/8251)
 const IMPORT_WORKER_HAMMERHEAD = `
 if (typeof importScripts !== "undefined" && /\\[native code]/g.test(importScripts.toString())) {
     var ${INSTRUCTION.getWorkerSettings} = function () {return ${WORKER_SETTINGS_PLACEHOLDER}};
