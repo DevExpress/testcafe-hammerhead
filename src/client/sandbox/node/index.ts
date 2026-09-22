d878c225f5a4967faff4a8326e4275cb78a09d2d
Aleksey28 <alexey.popov@devexpress.com>
2026-09-22 15:36:18 +0300

    ignored new-cap issues

--- src/client/sandbox/node/index.ts
+++ src/client/sandbox/node/index.ts
@@ -152,7 +152,7 @@ export default class NodeSandbox extends SandboxBase {
             // NOTE: Before overriding the iframe, we must restore native document methods.
             // Therefore, we save them before they are overridden.
             // @ts-ignore
-            const iframeNativeMethods = new this.nativeMethods.constructor(contentDocument, contentWindow);
+            const iframeNativeMethods = new this.nativeMethods.constructor(contentDocument, contentWindow); // eslint-disable-line new-cap
 
             contentWindow[INTERNAL_PROPS.iframeNativeMethods] = iframeNativeMethods;
 
